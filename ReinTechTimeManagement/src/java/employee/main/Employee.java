01df8c8b81274be493f73a181c7b9e6514b8c1b8
alexmaggard <amaggard09@gmail.com>
2017-11-06 20:47:40 -0800

    employee.java fix

--- ReinTechTimeManagement/src/java/employee/main/Employee.java
+++ ReinTechTimeManagement/src/java/employee/main/Employee.java
@@ -21,29 +21,8 @@ public class Employee implements Serializable{
     private int authLevel;
     private String status;
     private double payRate;
-    
-<<<<<<< HEAD
+   
     public Employee(){}
-=======
-    // Date Variables
-    //updated date variables
-    private Date day;
-    private DATETIME startTime;
-    private DATETIME lunchOut;
-    private DATETIME lunchIn;
-    private DATEIME outTime;
-    
-    //changing variables to match database revisions - Alex 11/3 9:25AM
-    /*private Date wedOut;
-    private Date thurIn;
-    private Date thurOut;
-    private Date friIn;
-    private Date friOut;*/
-    
-    public Employee(){
-        
-    }
->>>>>>> 7dfae34133e38b14a4bcd7754261d581fc1197e4
     
     //TODO: redo constructor with updated variables
     //TODO: remove unused accessor/mutator methods
